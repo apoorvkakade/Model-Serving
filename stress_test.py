cbe423e6fc48ee0491e111282276a6016ad53e04
Ubuntu <azureuser@VM1.wa0ioi4cegyupednwzhmycq5wd.phxx.internal.cloudapp.net>
2023-04-08 21:23:22 +0000

    Modified stress test to use thread pool

--- stress_test.py
+++ stress_test.py
@@ -1,35 +1,45 @@
 from threading import Thread
 import requests
 import time
+from time import sleep, perf_counter
+from concurrent.futures import ThreadPoolExecutor, as_completed
+ 
 # initialize the Keras REST API endpoint URL along with the input
 # image path
 AZURE_VM_IP = "20.25.160.84"
 
 KERAS_REST_API_URL = "http://" + AZURE_VM_IP + "/predict"
-IMAGE_PATH = "jemma.png"
+IMAGE_PATH = "fish.png"
 # initialize the number of requests for the stress test along with
 # the sleep amount between requests
 NUM_REQUESTS = 500
 SLEEP_COUNT = 0.05
+image = open(IMAGE_PATH, "rb").read()
 def call_predict_endpoint(n):
 	# load the input image and construct the payload for the request
-	image = open(IMAGE_PATH, "rb").read()
 	payload = {"image": image}
 	# submit the request
 	r = requests.post(KERAS_REST_API_URL, files=payload).json()
 	# ensure the request was sucessful
+	sleep(SLEEP_COUNT)
 	if r["success"]:
-		print("[INFO] thread {} OK".format(n))
+		return "[INFO] thread {} OK".format(n)
 	# otherwise, the request failed
 	else:
-		print("[INFO] thread {} FAILED".format(n))
+		return "[INFO] thread {} FAILED".format(n)
+	
 # loop over the number of threads
-for i in range(0, NUM_REQUESTS):
-	# start a new thread to call the API
-	t = Thread(target=call_predict_endpoint, args=(i,))
-	t.daemon = True
-	t.start()
-	time.sleep(SLEEP_COUNT)
-# insert a long sleep so we can wait until the server is finished
-# processing the images
-time.sleep(300)
+
+thread_results = []
+start_time = perf_counter()
+
+with ThreadPoolExecutor() as executor:
+	for i in range(0, NUM_REQUESTS):
+		thread_results.append(executor.submit(call_predict_endpoint, i))
+	
+	for res in as_completed(thread_results):
+		print(res.result())
+
+end_time = perf_counter()
+
+print("It took total time " end_time-start_time)
